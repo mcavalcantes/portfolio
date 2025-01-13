1c1d4ad01b70ec7a3518a1ed420ef5aba5a8d332
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-01-13 02:25:01 -0300

    added a link to my blog on the header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -37,23 +37,28 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col items-center px-8 md:px-32 xl:px-96">
       <header className="h-20 w-full flex items-center justify-end gap-2">
-        {
-          theme === "dark" ?
+        <div className="flex items-center gap-2">
+          {
+            theme === "dark" ?
 
-          <button onClick={handleClick} className="h-8 w-8 flex items-center justify-center rounded border border-[var(--border)] hover:bg-[var(--hover)] transition">
-            <SunIcon />
-          </button>
+            <button onClick={handleClick} className="h-8 w-8 flex items-center justify-center rounded border border-[var(--border)] hover:bg-[var(--hover)] transition">
+              <SunIcon />
+            </button>
 
-          :
+            :
 
-          <button onClick={handleClick} className="h-8 w-8 flex items-center justify-center rounded border border-[var(--border)] hover:bg-[var(--hover)] transition">
-            <MoonIcon />
-          </button>
-        }
-        <a href="/curriculo.pdf" className="h-8 w-28 flex items-center justify-evenly rounded bg-[var(--foreground)] text-[var(--foreground-text)] hover:bg-[var(--hover-foreground)] transition">
-          <DownloadIcon />
-          <p>Currículo</p>
-        </a>
+            <button onClick={handleClick} className="h-8 w-8 flex items-center justify-center rounded border border-[var(--border)] hover:bg-[var(--hover)] transition">
+              <MoonIcon />
+            </button>
+          }
+          <a className="h-8 w-12 flex items-center justify-center rounded border border-[var(--border)] hover:bg-[var(--hover)] transition" href="https://blog-mcavalcante.vercel.app/">
+            Blog
+          </a>
+          <a href="/curriculo.pdf" className="h-8 w-28 flex items-center justify-evenly rounded bg-[var(--foreground)] text-[var(--foreground-text)] hover:bg-[var(--hover-foreground)] transition">
+            <DownloadIcon />
+            <p>Currículo</p>
+          </a>
+        </div>
       </header>
       {children}
       <footer className="h-20 w-full text-sm flex items-center justify-center pointer-events-none select-none">
