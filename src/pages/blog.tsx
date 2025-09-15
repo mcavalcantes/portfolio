7519fe2f2ed7624c2ca625b18d16286ac232110e
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-09-15 13:02:38 -0300

    built blog landing page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,3 +1,17 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
 export function Blog() {
-  return <div>blog page</div>;
+  return (
+    <div className="flex min-h-screen flex-col bg-yellow-50 text-zinc-950 dark:bg-stone-900 dark:text-stone-200">
+      <Header />
+      <div className="flex flex-col gap-8 px-4 md:px-32 xl:px-96">
+        <div>
+          <h1 className="py-4 text-3xl font-semibold">Blog</h1>
+        </div>
+        <p>Em construção...</p>
+      </div>
+      <Footer />
+    </div>
+  );
 }
